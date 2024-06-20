47fd3c75f937a873e6edf9dd765a4ba7a5d1456e
Wei Mo <miloweimo@outlook.com>
2024-06-20 16:22:08 +0800

    ci: 本地服务器测试
    Signed-off-by: Wei Mo <miloweimo@outlook.com>

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-import express from 'express'
+const express = require('express')
 const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line
-  console.log(`server started on port ${PORT}`)
+  console.log(`server started on port ${PORT}`, `http://localhost:${PORT}`)
 })
